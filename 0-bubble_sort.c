9a1e045a5cc17ed71f7f81a291f0d6d773e70315
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-22 04:46:24 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,5 @@
 #include "sort.h"
-/**
- *swap - The function that swaps two integers
- *placed in wrong order.
- *@xp: Pointer to the first integer.
- *@yp: Pointer to the second integer.
- *Return: NULL.
- */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
+
 /**
  *bubble_sort - The function that sorts an
  *array of integers in ascending order
@@ -22,19 +10,28 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swaped;
+	int temp;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swaped = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
 				print_array(array, size);
+				swaped = 1;
 			}
+		}
+		if (swaped == 0){
+			break;
 		}
 	}
 }
