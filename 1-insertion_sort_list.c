82aa7aaf9a678fd5fee09564e9f6f9ecef0ec0db
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-22 05:18:31 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,40 +1,96 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list
- *	of integers in ascending order
- *	using the Insertion sort algorithm.
- * @list: Double pointer to the head of the doubly linked list.
+ * swap_left -swap two nodes right left position
+ * @list: list
+ *Return: NULL
  *
- * Description: The function sorts the doubly linked list using
- *	the Insertion sort  algorithm and swaps the nodes
- *	themselves (not the integer values).
- *   It prints the list after each swap of two elements.
+ **/
+void swap_left(listint_t *list)
+{
+	listint_t *tmp, *head;
+
+	while (list->prev != NULL)
+	{
+		if (list->n < list->prev->n)
+		{
+			tmp = list->prev->prev;
+			list->prev->next = list->next;
+			list->next = list->prev;
+			list->prev->prev = list;
+			list->prev = tmp;
+			list->next->next->prev = list->next;
+			if (tmp != NULL)
+				tmp->next = list;
+			head = list;
+			while (head->prev != NULL)
+				head = head->prev;
+			print_list(head);
+		}
+		else
+			list = list->prev;
+	}
+}
+
+/**
+ * swap_right -swap two nodes left rigth position
+ * @l: list
+ * Return: Null
+ **/
+void swap_right(listint_t *l)
+{
+	listint_t *tmp, *head;
+
+	tmp = l->prev;
+
+	if (tmp != NULL)
+	{
+		tmp->next = l->next;
+		l->next->prev = tmp;
+	}
+	else
+		l->next->prev = NULL;
+	l->prev = l->next;
+	if (l->next->next != NULL)
+	{
+		l->next = l->next->next;
+		l->prev->next = l;
+		l->next->prev = l;
+	}
+	else
+	{
+		l->next->next = l;
+		l->next = NULL;
+	}
+	head = l;
+	while (head->prev != NULL)
+		head = head->prev;
+	print_list(head);
+	swap_left(l->prev);
+}
+
+/**
+ * insertion_sort_list -sorts a doubly linked list in ascending order
+ * @list: list
+ * Return: NULL
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	listint_t *l;
+
+	if ((list == NULL) || (*list == NULL) || ((*list)->next == NULL))
 		return;
+	l = *list;
 
-	listint_t *current, *temp;
-
-	for (current = (*list)->next; current != NULL; current = current->next)
+	while (l->next != NULL)
 	{
-		temp = current;
-		while (temp->prev != NULL && temp->n < temp->prev->n)
+		if (l->n > l->next->n)
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
-
-			if (temp->prev == NULL)
-				*list = temp;
-			else
-				temp->prev->next = temp;
-			print_list(*list);
+			swap_right(l);
 		}
+		else
+			l = l->next;
 	}
+	while ((*list)->prev != NULL)
+		*list = (*list)->prev;
 }
