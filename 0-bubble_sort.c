ec0741289e11900c5763a46e69fd26359147aaa1
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-22 05:00:55 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,19 +24,24 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swaping;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swaping = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				swaping = 1;
 			}
 		}
+		if (swaping == 0)
+			break;
 	}
 }
