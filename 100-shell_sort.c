868d0d57e6b792cd46b43f0036e5be7901788ec6
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-22 06:26:44 -0700

    shell sory

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -5,7 +5,6 @@
  *	using the Shell sort algorithm with Knuth sequence.
  * @array: The array to be sorted.
  * @size: Size of the array.
- *
  * Description: The function sorts the array using the Shell sort algorithm
  *	with the Knuth sequence and prints the array each time it
  *	decreases the interval.
@@ -16,33 +15,25 @@ void shell_sort(int *array, size_t size)
 		return;
 	size_t interval = 1;
 
-	/**
-	 * Calculate the initial interval using the Knuth sequence
-	 */
 	while (interval < size / 3)
 		interval = interval * 3 + 1;
-
-	while (interval > 0)
+	while (interval >= 1)
 	{
 		size_t i, j;
 		int temp;
 
-		/**
-		 * Shell insertion sort with the current interval
-		 */
 		for (i = interval; i < size; i++)
 		{
 			temp = array[i];
-
-			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+			j = i;
+			while (j >= interval && array[j - interval] > temp)
+			{
 				array[j] = array[j - interval];
+				j -= interval;
+			}
 			array[j] = temp;
 		}
 		print_array(array, size);
-
-		/**
-		 * Decrease the interval using the Knuth sequence
-		 */
 		interval = (interval - 1) / 3;
 	}
 }
